889e5f50beb5cfe5a6354338d840187f4ec8fcdf
lewis.birks <lewisb@kainos.com>
2021-12-03 12:46:24 +0000

    feat: day 3 2021

--- src/main/java/git/lewisbirks/adventofcode/year21/Year2021.java
+++ src/main/java/git/lewisbirks/adventofcode/year21/Year2021.java
@@ -3,13 +3,14 @@ package git.lewisbirks.adventofcode.year21;
 import git.lewisbirks.adventofcode.common.Year;
 import git.lewisbirks.adventofcode.year21.day.Day1;
 import git.lewisbirks.adventofcode.year21.day.Day2;
+import git.lewisbirks.adventofcode.year21.day.Day3;
 import git.lewisbirks.adventofcode.year21.day.DayOf2021;
 
 import java.util.List;
 
 public class Year2021 extends Year<DayOf2021> {
 
-  private static final List<DayOf2021> DAYS = List.of(new Day1(), new Day2());
+  private static final List<DayOf2021> DAYS = List.of(new Day1(), new Day2(), new Day3());
 
   public Year2021() {
     super(2021);
