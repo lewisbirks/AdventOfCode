0d5c7cbede7b00977ec969321e74089ace42f922
lewisbirks <birkslewis@gmail.com>
2021-12-04 19:32:40 +0000

    refactor: delete old project structure

--- src/main/java/git/lewisbirks/adventofcode/year21/day/DayOf2021.java
+++
@@ -1,9 +0,0 @@
-package git.lewisbirks.adventofcode.year21.day;
-
-import git.lewisbirks.adventofcode.common.Day;
-
-public abstract class DayOf2021 extends Day {
-    public DayOf2021(int num) {
-        super(num, "2021");
-    }
-}
