37a8215cf77211378d80ceb0ce4a6c9e8b1e77b0
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2021-12-06 13:28:39 +0000

    feat: day 6 2021 (#5)

---
+++ adventofcode.year2021/src/main/java/com/lewisbirks/adventofcode/day/Day6.java
@@ -0,0 +1,48 @@
+package com.lewisbirks.adventofcode.day;
+
+import com.lewisbirks.adventofcode.common.cache.CachedSupplier;
+
+import java.util.Arrays;
+import java.util.function.Supplier;
+
+public class Day6 extends DayOf2021 {
+    private static final int NEW_LIFE_TIMER_START = 8;
+    private static final int STANDARD_LIFE_TIMER_START = 6;
+    private final Supplier<long[]> fishSupplier;
+
+    public Day6() {
+        super(STANDARD_LIFE_TIMER_START);
+        fishSupplier = CachedSupplier.memoize(
+            () -> {
+                long[] numFishPerAge = new long[NEW_LIFE_TIMER_START + 1];
+                for (String s : readInput().split(",")) {
+                    numFishPerAge[Integer.parseInt(s)]++;
+                }
+                return numFishPerAge;
+            }
+        );
+    }
+
+    @Override
+    protected Object part1() {
+        return processLifeForDays(80);
+    }
+
+    @Override
+    protected Object part2() {
+        return processLifeForDays(256);
+    }
+
+    private long processLifeForDays(final int numOfDays) {
+        long[] numFishPerAge = fishSupplier.get();
+        int day = 0;
+        do {
+            long[] tmp = new long[NEW_LIFE_TIMER_START + 1];
+            System.arraycopy(numFishPerAge, 1, tmp, 0, NEW_LIFE_TIMER_START);
+            tmp[NEW_LIFE_TIMER_START] = numFishPerAge[0];
+            tmp[STANDARD_LIFE_TIMER_START] += numFishPerAge[0];
+            numFishPerAge = tmp;
+        } while (++day != numOfDays);
+        return Arrays.stream(numFishPerAge).sum();
+    }
+}
--- adventofcode.year2021/src/main/java/com/lewisbirks/adventofcode/year/Year2021.java
+++ adventofcode.year2021/src/main/java/com/lewisbirks/adventofcode/year/Year2021.java
@@ -6,13 +6,16 @@ import com.lewisbirks.adventofcode.day.Day2;
 import com.lewisbirks.adventofcode.day.Day3;
 import com.lewisbirks.adventofcode.day.Day4;
 import com.lewisbirks.adventofcode.day.Day5;
+import com.lewisbirks.adventofcode.day.Day6;
 import com.lewisbirks.adventofcode.day.DayOf2021;
 
 import java.util.List;
 
 public class Year2021 extends Year<DayOf2021> {
 
-    private static final List<DayOf2021> DAYS = List.of(new Day1(), new Day2(), new Day3(), new Day4(), new Day5());
+    private static final List<DayOf2021> DAYS = List.of(
+        new Day1(), new Day2(), new Day3(), new Day4(), new Day5(), new Day6()
+    );
 
     public Year2021() {
         super(2021);
