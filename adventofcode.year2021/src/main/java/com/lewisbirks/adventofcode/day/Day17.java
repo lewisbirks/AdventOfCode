5a4c0cf4566510cdbbac9b06efc447a2940c0c9c
lewis.birks <lewisb@kainos.com>
2021-12-17 13:12:45 +0000

    refactor: parsing input

--- adventofcode.year2021/src/main/java/com/lewisbirks/adventofcode/day/Day17.java
+++ adventofcode.year2021/src/main/java/com/lewisbirks/adventofcode/day/Day17.java
@@ -5,27 +5,18 @@ import com.lewisbirks.adventofcode.common.domain.Day;
 import com.lewisbirks.adventofcode.model.point.Point;
 
 import java.util.function.Supplier;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public final class Day17 extends Day {
 
-    private static final Pattern TARGET_PATTERN = Pattern.compile(
-        "target area: x=?(-?\\d+)\\.\\.?(-?\\d+), y=?(-?\\d+)\\.\\.?(-?\\d+)"
-    );
     private final Supplier<Target> targetSupplier;
 
     public Day17() {
         super(17, "Trick Shot");
 
         targetSupplier = CachedSupplier.memoize(() -> {
-            String input = readInput();
-            Matcher matcher = TARGET_PATTERN.matcher(input);
-            if (matcher.matches()) {
-                throw new IllegalArgumentException("Couldn't parse input: " + input);
-            }
-            int x1 = Integer.parseInt(matcher.group(1)), x2 = Integer.parseInt(matcher.group(2));
-            int y1 = Integer.parseInt(matcher.group(3)), y2 = Integer.parseInt(matcher.group(4));
+            String[] bounds = readInput().replaceAll("[^-|\\d]", " ").trim().split("\\s+");
+            int x1 = Integer.parseInt(bounds[0]), x2 = Integer.parseInt(bounds[1]);
+            int y1 = Integer.parseInt(bounds[2]), y2 = Integer.parseInt(bounds[3]);
             return new Target(
                 new Point(Math.min(x1, x2), Math.max(y1, y2)), new Point(Math.max(x1, x2), Math.min(y1, y2))
             );
@@ -37,7 +28,10 @@ public final class Day17 extends Day {
         // brute force
         Target target = targetSupplier.get();
         int maxY = 0;
-        for (int y = -1000; y < 1000; y++) {
+        // initial y cannot go greater than the abs value of the lowest point of the box + 1 as otherwise
+        // it would just skip over it on the step before it hits
+        int yBound = Math.abs(target.bottomRight().y()) + 1;
+        for (int y = 0; y < yBound; y++) {
             maxY = Math.max(maxY, maxHeight(target, y));
         }
 
@@ -64,9 +58,17 @@ public final class Day17 extends Day {
     protected Object part2() {
         // brute force
         Target target = targetSupplier.get();
+        // no point starting at a y velocity lower than the y lower bound of the target as it will just immediately
+        // skip once it reaches the next step
+        int yUpperBound = Math.abs(target.bottomRight().y()) + 1;
+        int yLowerBound = target.bottomRight.y();
+        // no point going faster than the x upper bound of the target
+        int xUpperBound = target.bottomRight().x() + 1;
+
         int sum = 0;
-        for (int x = 1; x < 5000; x++) {
-            for (int y = -1000; y < 1000; y++) {
+        // assume that the target is not behind us
+        for (int x = 0; x < xUpperBound; x++) {
+            for (int y = yLowerBound; y < yUpperBound; y++) {
                 if (reachesTarget(target, x, y)) {
                     sum++;
                 }
@@ -79,10 +81,10 @@ public final class Day17 extends Day {
     private boolean reachesTarget(Target target, int xVelocity, int yVelocity) {
         Point position = new Point(0, 0);
         while (true) {
-            if (target.isInTarget(position)) {
+            if (target.intersects(position)) {
                 return true;
             }
-            if (target.isPastTarget(position)) {
+            if (target.isPassedBy(position)) {
                 return false;
             }
             position = new Point(position.x() + xVelocity, position.y() + yVelocity);
@@ -97,14 +99,14 @@ public final class Day17 extends Day {
     }
 
     record Target(Point topLeft, Point bottomRight) {
-        public boolean isInTarget(Point point) {
+        public boolean intersects(Point point) {
             return point.x() >= topLeft.x()
                    && point.x() <= bottomRight.x()
                    && point.y() <= topLeft.y()
                    && point.y() >= bottomRight.y();
         }
 
-        public boolean isPastTarget(Point point) {
+        public boolean isPassedBy(Point point) {
             return point.x() > bottomRight.x() || point.y() < bottomRight.y();
         }
     }
