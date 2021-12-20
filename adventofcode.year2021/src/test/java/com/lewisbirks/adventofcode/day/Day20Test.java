dc4e1c8357a3169925ec61dd22e8f0b32ede9f37
lewis.birks <lewisb@kainos.com>
2021-12-20 19:05:30 +0000

    feat: day 20 2021 part 1 - damn bound checks will be the death of me - also flickering infinite images are the worst

---
+++ adventofcode.year2021/src/test/java/com/lewisbirks/adventofcode/day/Day20Test.java
@@ -0,0 +1,20 @@
+package com.lewisbirks.adventofcode.day;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day20Test {
+
+    private final Day20 underTest = new Day20();
+
+    @Test
+    void part1() {
+        assertThat(underTest.part1()).isEqualTo(35);
+    }
+
+    @Test
+    void part2() {
+    }
+}
