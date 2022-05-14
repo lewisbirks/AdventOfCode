51369f1b2bbd164d69a6f85c5f135b2da27fa9f4
lewis.birks <lewis.birks@defra.onmicrosoft.com>
2022-05-14 22:53:08 +0100

    refactor: move collection classes to common module

--- adventofcode.common/src/main/java/module-info.java
+++ adventofcode.common/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module adventofcode.common {
     exports com.lewisbirks.adventofcode.common.domain;
     exports com.lewisbirks.adventofcode.common.cache;
+    exports com.lewisbirks.adventofcode.collection;
 }
