35a0ac1713374822d0ff7f1bbae6d0440e0a2a50
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2022-12-06 10:54:00 +0000

    feat(2022): day 6 - Tuning Trouble (#49)

--- adventofcode.year2022/src/main/java/com/lewisbirks/adventofcode/day/Day6.java
+++ adventofcode.year2022/src/main/java/com/lewisbirks/adventofcode/day/Day6.java
@@ -2,25 +2,69 @@ package com.lewisbirks.adventofcode.day;
 
 import com.lewisbirks.adventofcode.common.domain.Day;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public final class Day6 extends Day {
 
+    private String input;
 
     public Day6() {
-        super(6, "");
+        super(6, "Tuning Trouble");
     }
 
     @Override
     protected void preLoad() {
-
+        input = readInput();
     }
 
     @Override
     protected Object part1() {
-        return null;
+        return findMarker(4);
     }
 
     @Override
     protected Object part2() {
-        return null;
+        return findMarker(14);
+    }
+
+    private int findMarker(int markerSize) {
+        int size = input.length() - 1;
+        for (int i = markerSize; i < size; i++) {
+            String subsection = input.substring(i - markerSize, i);
+            if (hasDistinctCharacters(subsection)) {
+                return i;
+            }
+            int addition = indexOfLastRepeatingChar(subsection);
+            i += addition;
+        }
+        throw new IllegalArgumentException("No marker found");
+    }
+
+    private static boolean hasDistinctCharacters(String subsection) {
+        Set<Character> found = new HashSet<>();
+        char[] chars = subsection.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (!found.add(chars[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int indexOfLastRepeatingChar(String subsection) {
+        Map<Character, Integer> found2 = new HashMap<>();
+        char[] chars = subsection.toCharArray();
+        int index = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (found2.containsKey(chars[i])) {
+                index = found2.get(chars[i]);
+            } else {
+                found2.put(chars[i], i);
+            }
+        }
+        return index;
     }
 }
--- adventofcode.year2022/src/test/java/com/lewisbirks/adventofcode/day/Day6Test.java
+++ adventofcode.year2022/src/test/java/com/lewisbirks/adventofcode/day/Day6Test.java
@@ -2,19 +2,25 @@ package com.lewisbirks.adventofcode.day;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Day6Test {
 
     private final Day6 underTest = new Day6();
 
+    @BeforeEach
+    void setUp() {
+        underTest.preLoad();
+    }
+
     @Test
     void part1() {
-        assertThat(underTest.part1()).isNull();
+        assertThat(underTest.part1()).isEqualTo(7);
     }
 
     @Test
     void part2() {
-        assertThat(underTest.part2()).isNull();
+        assertThat(underTest.part2()).isEqualTo(19);
     }
 }
