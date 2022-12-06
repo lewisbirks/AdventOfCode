746481717e686e4f100984d4cb94c5aa36595370
lewisbirks <birkslewis@gmail.com>
2022-12-06 09:36:17 +0000

    feat: day 6 part 1

--- adventofcode.year2022/src/main/java/com/lewisbirks/adventofcode/day/Day6.java
+++ adventofcode.year2022/src/main/java/com/lewisbirks/adventofcode/day/Day6.java
@@ -4,19 +4,26 @@ import com.lewisbirks.adventofcode.common.domain.Day;
 
 public final class Day6 extends Day {
 
+    private String input;
 
     public Day6() {
-        super(6, "");
+        super(6, "Tuning Trouble");
     }
 
     @Override
     protected void preLoad() {
-
+        input = readInput();
     }
 
     @Override
     protected Object part1() {
-        return null;
+        for (int i = 0; i < input.length() - 3; i++) {
+            String subsection = input.substring(i, i + 4);
+            if (subsection.chars().distinct().count() == 4) {
+                return i + 4;
+            }
+        }
+        throw new IllegalArgumentException("No marker found");
     }
 
     @Override
--- adventofcode.year2022/src/test/java/com/lewisbirks/adventofcode/day/Day6Test.java
+++ adventofcode.year2022/src/test/java/com/lewisbirks/adventofcode/day/Day6Test.java
@@ -2,15 +2,21 @@ package com.lewisbirks.adventofcode.day;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Day6Test {
 
     private final Day6 underTest = new Day6();
 
+    @BeforeEach
+    void setUp() {
+        underTest.preLoad();
+    }
+
     @Test
     void part1() {
-        assertThat(underTest.part1()).isNull();
+        assertThat(underTest.part1()).isEqualTo(7);
     }
 
     @Test
