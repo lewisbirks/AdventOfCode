1391da91b201b49f422ad7990ba3f70dd7586aed
lewisbirks <birkslewis@gmail.com>
2022-12-07 12:48:10 +0000

    refactor: make test fail and skip for the skeleton

--- adventofcode.year2022/src/test/java/com/lewisbirks/adventofcode/day/Day10Test.java
+++ adventofcode.year2022/src/test/java/com/lewisbirks/adventofcode/day/Day10Test.java
@@ -2,6 +2,7 @@ package com.lewisbirks.adventofcode.day;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class Day10Test {
@@ -9,12 +10,14 @@ class Day10Test {
     private final Day10 underTest = new Day10();
 
     @Test
+    @Disabled
     void part1() {
-        assertThat(underTest.part1()).isNull();
+        assertThat(underTest.part1()).isNotNull();
     }
 
     @Test
+    @Disabled
     void part2() {
-        assertThat(underTest.part2()).isNull();
+        assertThat(underTest.part2()).isNotNull();
     }
 }
--- adventofcode.year2022/src/test/java/com/lewisbirks/adventofcode/day/Day19Test.java
+++ adventofcode.year2022/src/test/java/com/lewisbirks/adventofcode/day/Day19Test.java
@@ -2,6 +2,7 @@ package com.lewisbirks.adventofcode.day;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class Day19Test {
@@ -9,12 +10,14 @@ class Day19Test {
     private final Day19 underTest = new Day19();
 
     @Test
+    @Disabled
     void part1() {
-        assertThat(underTest.part1()).isNull();
+        assertThat(underTest.part1()).isNotNull();
     }
 
     @Test
+    @Disabled
     void part2() {
-        assertThat(underTest.part2()).isNull();
+        assertThat(underTest.part2()).isNotNull();
     }
 }
