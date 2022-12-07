507b61c99b49a568964123e4241773be0b2bb402
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2022-12-07 12:49:59 +0000

    refactor: make test fail and skip for the skeleton (#51)

--- adventofcode.year2022/src/test/java/com/lewisbirks/adventofcode/day/Day7Test.java
+++ adventofcode.year2022/src/test/java/com/lewisbirks/adventofcode/day/Day7Test.java
@@ -2,6 +2,7 @@ package com.lewisbirks.adventofcode.day;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class Day7Test {
@@ -9,12 +10,14 @@ class Day7Test {
     private final Day7 underTest = new Day7();
 
     @Test
+    @Disabled
     void part1() {
-        assertThat(underTest.part1()).isNull();
+        assertThat(underTest.part1()).isNotNull();
     }
 
     @Test
+    @Disabled
     void part2() {
-        assertThat(underTest.part2()).isNull();
+        assertThat(underTest.part2()).isNotNull();
     }
 }
