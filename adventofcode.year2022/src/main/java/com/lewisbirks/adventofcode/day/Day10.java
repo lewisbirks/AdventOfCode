320748ab6745c9b30215c33d9e3c4479c3c54b2b
lewisbirks <birkslewis@gmail.com>
2022-12-13 12:07:54 +0000

    feat: day 10 part 1

--- adventofcode.year2022/src/main/java/com/lewisbirks/adventofcode/day/Day10.java
+++ adventofcode.year2022/src/main/java/com/lewisbirks/adventofcode/day/Day10.java
@@ -2,21 +2,51 @@ package com.lewisbirks.adventofcode.day;
 
 import com.lewisbirks.adventofcode.common.domain.Day;
 
+import java.util.List;
+
 public final class Day10 extends Day {
 
+    private static final int PROCESSING_TIME = 2;
+
+    private List<String> instructions;
 
     public Day10() {
-        super(10, "");
+        super(10, "Cathode-Ray Tube");
     }
 
     @Override
     protected void preLoad() {
-
+        instructions = getInput();
     }
 
     @Override
     protected Object part1() {
-        return null;
+        int[] cycleChecks = {20, 60, 100, 140, 180, 220};
+        int addition = 0;
+        int processing = -1;
+        int x = 1;
+        long signalStrength = 0;
+        int checkIndex = 0;
+        for (int i = 0, j = 0; i < 220; i++) {
+            if (i + 1 == cycleChecks[checkIndex]) {
+                signalStrength += (long) cycleChecks[checkIndex] * x;
+                checkIndex++;
+            }
+            if (processing == -1) {
+                String instruction = instructions.get(j++);
+                if (instruction.startsWith("add")) {
+                    addition = Integer.parseInt(instruction.substring(5));
+                    processing = PROCESSING_TIME;
+                }
+            }
+            if (processing != -1) {
+                if (--processing == 0) {
+                    x += addition;
+                    processing = -1;
+                }
+            }
+        }
+        return signalStrength;
     }
 
     @Override
--- adventofcode.year2022/src/test/java/com/lewisbirks/adventofcode/day/Day10Test.java
+++ adventofcode.year2022/src/test/java/com/lewisbirks/adventofcode/day/Day10Test.java
@@ -2,6 +2,7 @@ package com.lewisbirks.adventofcode.day;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -9,10 +10,15 @@ class Day10Test {
 
     private final Day10 underTest = new Day10();
 
+    @BeforeEach
+    void setUp() {
+        underTest.preLoad();
+    }
+
     @Test
     @Disabled
     void part1() {
-        assertThat(underTest.part1()).isNotNull();
+        assertThat(underTest.part1()).isEqualTo(13140L);
     }
 
     @Test
