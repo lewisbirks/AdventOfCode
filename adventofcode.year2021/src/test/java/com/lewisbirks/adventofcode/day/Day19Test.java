cf62439d0c2810c60c3c130e4cfecd6d95f6963a
lewisbirks <birkslewis@gmail.com>
2022-12-22 18:15:34 +0000

    fixup! refactor: remove caching 2021

--- adventofcode.year2021/src/test/java/com/lewisbirks/adventofcode/day/Day19Test.java
+++ adventofcode.year2021/src/test/java/com/lewisbirks/adventofcode/day/Day19Test.java
@@ -3,9 +3,12 @@ package com.lewisbirks.adventofcode.day;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.TestInstance.Lifecycle;
 
+@TestInstance(Lifecycle.PER_CLASS)
 class Day19Test {
 
     private final Day19 underTest = new Day19();
