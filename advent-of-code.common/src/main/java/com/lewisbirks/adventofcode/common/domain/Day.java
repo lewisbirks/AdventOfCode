ecc5823bb87b09b580065126926589c7fefc70d6
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2023-12-09 14:45:35 +0000

    chore: remove custom performance analysis (#87)

--- advent-of-code.common/src/main/java/com/lewisbirks/adventofcode/common/domain/Day.java
+++ advent-of-code.common/src/main/java/com/lewisbirks/adventofcode/common/domain/Day.java
@@ -3,93 +3,53 @@ package com.lewisbirks.adventofcode.common.domain;
 import static com.lewisbirks.adventofcode.common.utils.Time.nanoRoundToString;
 
 import com.lewisbirks.adventofcode.common.resource.ResourceUtil;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.LongSummaryStatistics;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import org.apache.commons.lang3.time.DurationFormatUtils;
 
 public abstract class Day implements Comparable<Day> {
 
     private final int num;
     private final String name;
 
-    private final boolean performance;
-    private final int iterations;
     protected final boolean debug;
 
     public Day(int num, String name) {
         this.num = num;
         this.name = name;
         this.debug = Boolean.getBoolean("debug");
-        this.performance = Boolean.getBoolean("performance");
-        this.iterations = this.performance ? Integer.parseInt(System.getProperty("iterations", "100")) : 1;
     }
 
     public void process() {
-        int iterations = performance ? this.iterations : 1;
         System.out.printf("Day %02d: %s%n", num, name);
         try {
             long start, end;
-            Object result = null;
-            List<Long> timings = new ArrayList<>();
+            Object result;
 
-            for (int i = 0; i < iterations; i++) {
-                preload();
-                if (performance) {
-                    System.out.printf("\r\tIteration: %d", i + 1);
-                }
-                start = System.nanoTime();
-                result = part1();
-                end = System.nanoTime();
-                if (result == null) {
-                    System.out.println("\r\tNot implemented... skipping");
-                    return;
-                }
-                timings.add(end - start);
+            preload();
+            start = System.nanoTime();
+            result = part1();
+            end = System.nanoTime();
+            if (result == null) {
+                System.out.println("\r\tNot implemented... skipping");
+                return;
             }
-            printResults(1, result, timings);
+            System.out.printf("\r\tPart %d: %s (%s)%n", 1, result, nanoRoundToString(end - start));
 
-            timings = new ArrayList<>();
-            for (int i = 0; i < iterations; i++) {
-                preload();
-                if (performance) {
-                    System.out.printf("\r\tIteration: %d", i + 1);
-                }
-                start = System.nanoTime();
-                result = part2();
-                end = System.nanoTime();
-                if (result == null) {
-                    System.out.println("\r\tNot implemented... skipping");
-                    return;
-                }
-                timings.add(end - start);
+            preload();
+            start = System.nanoTime();
+            result = part2();
+            end = System.nanoTime();
+            if (result == null) {
+                System.out.println("\r\tNot implemented... skipping");
+                return;
             }
-            printResults(2, result, timings);
+            System.out.printf("\r\tPart %d: %s (%s)%n", 2, result, nanoRoundToString(end - start));
         } catch (Exception e) {
             System.err.printf("Failed to process day %02d%n", num);
             e.printStackTrace();
-        }
-    }
-
-    private void printResults(int part, Object result, List<Long> timings) {
-        if (performance) {
-            LongSummaryStatistics stats = timings.stream().mapToLong(l -> l).summaryStatistics();
-            long totalMilli = stats.getSum() / 1000_000;
-            String extra = "runs: %d, avg: %s, min: %s, max: %s, total: %s"
-                    .formatted(
-                            stats.getCount(),
-                            nanoRoundToString((long) stats.getAverage()),
-                            nanoRoundToString(stats.getMin()),
-                            nanoRoundToString(stats.getMax()),
-                            DurationFormatUtils.formatDuration(totalMilli, "mm:ss.SSS"));
-            System.out.printf("\r\tPart %d: %s (%s)%n", part, result, extra);
-        } else {
-            System.out.printf(
-                    "\r\tPart %d: %s (%s)%n", part, result, nanoRoundToString(timings.get(timings.size() - 1)));
         }
     }
 
