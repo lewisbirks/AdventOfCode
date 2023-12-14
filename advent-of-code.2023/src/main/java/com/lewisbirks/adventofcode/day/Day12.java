6c80c901a0234c5fb9c378dad07d6c0c862f2810
lewisbirks <birkslewis@gmail.com>
2023-12-14 18:40:46 +0000

    setup

--- advent-of-code.2023/src/main/java/com/lewisbirks/adventofcode/day/Day12.java
+++ advent-of-code.2023/src/main/java/com/lewisbirks/adventofcode/day/Day12.java
@@ -1,23 +1,53 @@
 package com.lewisbirks.adventofcode.day;
 
 import com.lewisbirks.adventofcode.common.domain.Day;
+import java.util.Arrays;
+import java.util.List;
 
 public final class Day12 extends Day {
 
+    public static void main(String[] args) {
+
+    }
+
+    private List<Springs> field;
+
     public Day12() {
-        super(12, "");
+        super(12, "Hot Springs");
     }
 
     @Override
-    public void preload() {}
+    public void preload() {
+        field = getInput(line -> {
+            String[] parts = line.split(" ");
+            int[] counts = Arrays.stream(parts[1].split(",")).mapToInt(Integer::parseInt).toArray();
+            return new Springs(parts[0].toCharArray(), counts);
+        });
+    }
 
     @Override
     public Object part1() {
-        return null;
+        return field.stream().mapToInt(Springs::possibleArrangements).sum();
     }
 
     @Override
     public Object part2() {
         return null;
+    }
+
+    record Springs(char[] springs, int[] counts) {
+
+        public int possibleArrangements() {
+            return -1;
+        }
+
+        private boolean isPlaceable(int index, int count, char[] springs) {
+            for (int i = index + 1; i < index + count; i++) {
+                if (springs[i] == '#') {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
